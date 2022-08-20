90d39293ad216643441a9f5d9aedd09bc5902df5
shawon ahmmed <shawonahmmed08509@gmail.com>
2022-08-20 12:07:33 +0600

    multer installed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const multer = require('multer');
 app.use(cors());
 app.use(express.json());
 require('dotenv').config();
@@ -14,6 +15,14 @@ app.get('/', (req, res) =>{
     res.send("fast commerce server")
 })
 
+
+// file upload function
+const uploads_folder = './uploads/';
+const upload = multer({
+  dest: uploads_folder
+})
+
+
 // Replace the uri string with your connection string.
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gv57l.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri);
@@ -22,6 +31,16 @@ async function run() {
     await client.connect();
     const database = client.db('FastCommerce');
     const productCollection = database.collection('products');
+
+    app.post('/addproduct', upload.single("image"), (req, res)=>{
+      const data = req.file;
+
+      console.log("data from clientside", req.body)
+      console.log("image from clientside", data)
+      res.json("data recieved")
+    })
+
+
     
   } finally {
     console.log("server operation complete")
